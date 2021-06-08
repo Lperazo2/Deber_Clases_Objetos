17712986172faf5fd138d8c82d911c44235bc13c
Leonardo Erazo <leonardoerazolpea1998@gmail.com>
2021-06-08 01:18:12 -0500

    Tarea de clases y objetos en java

--- src/clases/cubo.java
+++ src/clases/cubo.java
@@ -8,7 +8,7 @@ public class cubo {
 
     public void volumen() {
         int respuesta = ancho * alto * profundo;
-        System.out.println("El cubo a calcular tiene los siguientes datos");
+        System.out.println("El cubo a calcular tiene los siguientes datos ");
         System.out.println("Altura = " + alto);
         System.out.println("Ancho = " + ancho);
         System.out.println("Profundidad = " + profundo);
--- src/clases/matematicas.java
+++ src/clases/matematicas.java
@@ -15,5 +15,6 @@ public class matematicas {
         ob2.lado=12;
         ob2.area();
     }
-   
+    
+    
 }
